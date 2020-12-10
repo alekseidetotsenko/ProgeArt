f7fe52ea9c9b17472b37a8d50bd09be23070885c
alekseidetotsenko <73224030+alekseidetotsenko@users.noreply.github.com>
2020-12-11 00:06:42 +0200

    ä

--- GUI/ALX_proov/ProgeArtAlx.py
+++ GUI/ALX_proov/ProgeArtAlx.py
@@ -7,8 +7,8 @@ list_of_colors = [(145, 185, 141), (229, 192, 121), (210, 191, 88), (140, 190, 1
  (41, 241, 195), (243, 156, 18), (189, 195, 199), (101, 198, 187), (255, 246, 143), (243, 241, 239)]
 
 
-WIDTH = 1980
-HEIGHT = 1020
+WIDTH = 3*1980
+HEIGHT = 2*1020
 surface = cairo.ImageSurface(cairo.FORMAT_RGB24, WIDTH, HEIGHT)
 
 ctx = cairo.Context(surface)
@@ -24,7 +24,7 @@ def dontknowyet(ctx,WIDTH,HEIGHT):
     b = random.randint(10,50)
     for j in range(a):
         algus_x = random.randint(int(-2000 * x_telje_suhe), int(7000 * x_telje_suhe))
-        algus_y = random.randint(int(-2000 * y_telje_suhe), int(10000 * y_telje_suhe))
+        algus_y = random.randint(int(-2000 * x_telje_suhe), int(10000 * y_telje_suhe))
         ctx.move_to(algus_x, algus_y)
         line_color = random.choice(list_of_colors)
         line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
@@ -36,35 +36,16 @@ def dontknowyet(ctx,WIDTH,HEIGHT):
             for k in range(b):
                 ctx.line_to(algus_x,algus_y)
                 ctx.rel_line_to(random.randrange(-5,5),random.randrange(-5,5))
-            ctx.stroke()
+            ctx.stroke()           
             
-def dontknowyet(ctx,WIDTH,HEIGHT):
+def dontknowyet2(ctx,WIDTH,HEIGHT):
     x_telje_suhe = WIDTH / (3*1980)
     y_telje_suhe = HEIGHT / (3*1020)
     a = random.randint(100,3000)
-    b = random.randint(10,50)
-    for j in range(a):
-        algus_x = random.randint(-2000, 7000)
-        algus_y = random.randint(-2000, 10000)
-        ctx.move_to(algus_x, algus_y)
-        line_color = random.choice(list_of_colors)
-        line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
-        ctx.set_source_rgb(line_r, line_g, line_b)
-        ctx.set_line_width(random.randint(1,2))
-        for i in range(b):
-            ctx.line_to(algus_x,algus_y)
-            ctx.rel_line_to(random.randrange(-10,10),random.randrange(-10,10))
-            for k in range(b):
-                ctx.line_to(algus_x,algus_y)
-                ctx.rel_line_to(random.randrange(-5,5),random.randrange(-5,5))
-            ctx.stroke()
-            
-def dontknowyet2(ctx,WIDTH,HEIGHT):
-    a = random.randint(100,3000)
     b = random.randint(2,50)
     for j in range(a):
-        algus_x = random.randint(-2000, 7000)
-        algus_y = random.randint(-2000, 10000)
+        algus_x = random.randint(int(-2000 * x_telje_suhe), int(7000 * x_telje_suhe))
+        algus_y = random.randint(int(-2000 * y_telje_suhe), int(10000 * y_telje_suhe))
         ctx.move_to(algus_x, algus_y)
         line_color = random.choice(list_of_colors)
         line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
@@ -72,16 +53,18 @@ def dontknowyet2(ctx,WIDTH,HEIGHT):
         ctx.set_line_width(random.uniform(0.01,1))
         for k in range(b):
             ctx.move_to(algus_x, algus_y)
-            ctx.rel_line_to(algus_x,random.randrange(-50,50))
-            ctx.rel_line_to(random.randrange(-50,50),algus_y)
+            ctx.rel_line_to(algus_x,random.randrange(int(-50* x_telje_suhe),int(50* x_telje_suhe)))
+            ctx.rel_line_to(random.randrange(int(-50* y_telje_suhe),int(50* y_telje_suhe)),algus_y)
             ctx.stroke()
            
 def dontknowyet3(ctx,WIDTH,HEIGHT):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
     a = random.randint(100,3000)
     b = random.randint(2,50)
     for j in range(a):
-        algus_x = random.randint(-2000, 7000)
-        algus_y = random.randint(-2000, 10000)
+        algus_x = random.randint(int(-2000* x_telje_suhe), int(7000* x_telje_suhe))
+        algus_y = random.randint(int(-2000* y_telje_suhe), int(10000* y_telje_suhe))
         ctx.move_to(algus_x, algus_y)
         line_color = random.choice(list_of_colors)
         line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
@@ -90,27 +73,31 @@ def dontknowyet3(ctx,WIDTH,HEIGHT):
         ctx.stroke()
         for k in range(b):
             ctx.move_to(algus_x, algus_y)
-            ctx.line_to(algus_x,random.randrange(-50,50))
+            ctx.line_to(algus_x,random.randrange(int(-50* x_telje_suhe),int(50* x_telje_suhe)))
             ctx.stroke()
         for k in range(b):
             ctx.move_to(algus_x, algus_y)
-            ctx.line_to(random.randrange(-50,50),algus_y)
+            ctx.line_to(random.randrange(int(-50* y_telje_suhe),int(50* y_telje_suhe)),algus_y)
             ctx.stroke()
        
 def jooned(ctx,WIDTH,HEIGHT):
-    a = 3000
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
+    a = int(1500 * x_telje_suhe)
     for i in range(a):
-        ctx.line_to(random.randrange(3000), random.randrange(3000))
-        ctx.rel_line_to(random.randrange(1000), random.randrange(1000))
+        ctx.line_to(random.randrange(int(3000* x_telje_suhe)), random.randrange(int(3000* x_telje_suhe)))
+        ctx.rel_line_to(random.randrange(int(1000* y_telje_suhe)), random.randrange(int(1000* y_telje_suhe)))
         ctx.set_source_rgb(1, 1, 1)
         ctx.set_line_width(2)
         ctx.stroke()
     
 def hexagonid_värvilised(ctx,WIDTH,HEIGHT):
-    a = 2000
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
+    a = int(2000 * x_telje_suhe)
     for i in range(a):
-        algus_x = random.randint(-2000, 10000)
-        algus_y = random.randint(-2000, 10000)
+        algus_x = random.randint(int(-2000* x_telje_suhe), int(10000* x_telje_suhe))
+        algus_y = random.randint(int(-2000* y_telje_suhe), int(10000* y_telje_suhe))
         suvaarv = random.randrange(0, 300, 5)
         HEX_W, HEX_H = (suvaarv, suvaarv)
         ctx.move_to(algus_x, algus_y)
@@ -131,10 +118,12 @@ def hexagonid_värvilised(ctx,WIDTH,HEIGHT):
         ctx.stroke ()
 
 def hexagonid_must_valge(ctx,WIDTH,HEIGHT):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
     a = 2000
     for i in range(1000):
-        algus_x = random.randint(-2000, 10000)
-        algus_y = random.randint(-2000, 10000)
+        algus_x = random.randint(int(-2000* x_telje_suhe), int(10000* x_telje_suhe))
+        algus_y = random.randint(int(-2000* y_telje_suhe), int(10000* y_telje_suhe))
         suvaarv = random.randrange(0, 300, 5)
         HEX_W, HEX_H = (suvaarv, suvaarv)
         ctx.move_to(algus_x, algus_y)
@@ -153,10 +142,12 @@ def hexagonid_must_valge(ctx,WIDTH,HEIGHT):
         ctx.stroke ()
 
 def jooned_nurgast(ctx,WIDTH,HEIGHT):
-    algus_x = random.randint(0, 2000)
-    algus_y = random.randint(0, 2000)
-    l6pp_x = random.randint(0, 5000)
-    l6pp_y = random.randint(0, 5000)
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
+    algus_x = random.randint(0, int(2000* x_telje_suhe))
+    algus_y = random.randint(0, int(2000* y_telje_suhe))
+    l6pp_x = random.randint(0, int(5000* x_telje_suhe))
+    l6pp_y = random.randint(0, int(5000* y_telje_suhe))
     
     for i in range(200):
         ctx.line_to(algus_x, algus_y)
@@ -168,17 +159,18 @@ def jooned_nurgast(ctx,WIDTH,HEIGHT):
         algus_x = l6pp_x 
         algus_y = l6pp_y 
         i -= 1
-        l6pp_x = random.randint(-2000, 3000)
-        l6pp_y = random.randint(-2000, 2000)
+        l6pp_x = random.randint(int(-2000* x_telje_suhe), int(3000* x_telje_suhe))
+        l6pp_y = random.randint(int(-2000* y_telje_suhe), int(2000* y_telje_suhe))
     ctx.close_path()    
     ctx.stroke()
 
 def joonedkeskelt(ctx,WIDTH,HEIGHT):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
     a = 150
-
     for i in range(a):
         ctx.line_to(WIDTH/2,HEIGHT/2)
-        ctx.rel_line_to(random.randint(-3000,3000),random.randint(-3000,3000))
+        ctx.rel_line_to(random.randint(int(-3000* x_telje_suhe),int(3000* x_telje_suhe)),random.randint(int(-3000* y_telje_suhe),int(3000* y_telje_suhe)))
         line_color = random.choice(list_of_colors)
         line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
         ctx.set_source_rgb(line_r, line_g, line_b)
@@ -186,36 +178,42 @@ def joonedkeskelt(ctx,WIDTH,HEIGHT):
         ctx.stroke()
 
 def ringid_valged(ctx,WIDTH,HEIGHT):
-    for i in range(20):
-        r = 10
-        x = random.randint(1,3*1980)
-        y = random.randint(1,3*1980)
-        for i in range(100):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
+    for i in range(int(20* x_telje_suhe)):
+        r = int(10* x_telje_suhe)
+        x = random.randint(1,int(3*1980* x_telje_suhe))
+        y = random.randint(1,int(3*1980* y_telje_suhe))
+        for i in range(int(100* x_telje_suhe)):
             ctx.set_source_rgb(1, 1, 1)
             ctx.arc(x, y, r, 0, 2*math.pi)
             ctx.set_line_width(2)
             ctx.stroke()
-            r += 6
-            x += random.randint(-50,50)
-            y += random.randint(-50,50)
+            r += int(6* x_telje_suhe)
+            x += random.randint(int(-50* x_telje_suhe),int(50* x_telje_suhe))
+            y += random.randint(int(-50* y_telje_suhe),int(50* y_telje_suhe))
             
 def ringid_värvilised(ctx,WIDTH,HEIGHT):
-    for i in range(20):
-        r = 10
-        x = random.randint(0,3*1980)
-        y = random.randint(0,3*1980)
-        for i in range(100):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
+    for i in range(int(20* x_telje_suhe)):
+        r = int(10* x_telje_suhe)
+        x = random.randint(1,int(3*1980* x_telje_suhe))
+        y = random.randint(1,int(3*1980* y_telje_suhe))
+        for i in range(int(100* x_telje_suhe)):
             line_color = random.choice(list_of_colors)
             line_r, line_g, line_b = line_color[0]/255.0, line_color[1]/255.0, line_color[2]/255.0
             ctx.set_source_rgb(line_r, line_g, line_b)
             ctx.arc(x, y, r, 0, 2*math.pi)
             ctx.set_line_width(2)
             ctx.stroke()
-            r += 6
-            x += random.randint(-50,50)
-            y += random.randint(-50,50)            
+            r += int(6* x_telje_suhe)
+            x += random.randint(int(-50* x_telje_suhe),int(50* x_telje_suhe))
+            y += random.randint(int(-50* y_telje_suhe),int(50* y_telje_suhe))           
             
 def spiderpuff(ctx,WIDTH,HEIGHT):
+    x_telje_suhe = WIDTH / (3*1980)
+    y_telje_suhe = HEIGHT / (3*1020)
     a = 50
     b = 5
     for j in range(a):
@@ -437,6 +435,6 @@ def midagit6(ctx,WIDTH,HEIGHT):
             ctx.restore()
             ctx.stroke()
             
-dontknowyet(ctx, 5000, 5000)
+ringid_valged(ctx, WIDTH, HEIGHT)
 
 surface.write_to_png('xxx.png')
