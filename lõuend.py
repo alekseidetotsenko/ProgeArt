fb07c81c30b7909849079bc831f40b140be9b6e1
karolinkonrad <karolinkonrad@hotmail.com>
2020-12-15 16:11:47 +0200

    ei kirjuta enam valmis pilte üle

--- lõuend.py
+++ lõuend.py
@@ -3,28 +3,38 @@ from kunst import *
 from kribukrabu import*
 import cairo
 from inspect import getmembers, isfunction # nende abil saab kujunduste faili funktsioonidest listi teha
+from os import listdir
 
 def lõuend(cr, muster, laius, kõrgus, taust, värv):
     if taust == 1:
         cr.set_source_rgb(värv[0][0]/255.0, värv[0][1]/255.0, värv[0][2]/255.0)
-    else:
-        cr.set_source_rgba(0, 0, 0, 0)
-    cr.rectangle(0, 0, laius, kõrgus)
-    cr.fill()
     
     eval(muster)(cr, laius, kõrgus) # valib funktsiooni ja annab sellele vajalikud argumendid
     
+def originaalne_nimi(failinimi, laiend):
+    valmis_tööd = listdir("Valminud kunstiteosed")
+    a = 0
+    uus_nimi = failinimi + laiend
+    while uus_nimi in valmis_tööd:
+         uus_nimi = failinimi + '(' + str(a) + ')'+ laiend
+         a += 1
+    return uus_nimi
+    
 # funktsioonid svg ja png loomiseks
 def svg_fail(muster, laius, kõrgus, faili_nimi, taust, värv):
-    pilt = cairo.SVGSurface('Valminud kunstiteosed/' +faili_nimi + '.svg', laius, kõrgus)
+    uus_nimi = originaalne_nimi(faili_nimi, '.svg')
+    
+    pilt = cairo.SVGSurface('Valminud kunstiteosed/' + uus_nimi, laius, kõrgus)
     c = cairo.Context(pilt)
     lõuend(c, muster, laius, kõrgus, taust, värv)#teeb pildi põhja ja kutsub välja vastava
     
     pilt.finish()
     
 def png_fail(muster, laius, kõrgus, faili_nimi, taust, värv):
+    uus_nimi = originaalne_nimi(faili_nimi, '.png')
+    
     pilt = cairo.ImageSurface(cairo.FORMAT_ARGB32, laius, kõrgus)
     c = cairo.Context(pilt)
     lõuend(c, muster, laius, kõrgus, taust, värv)
     
-    pilt.write_to_png('Valminud kunstiteosed/' +faili_nimi + '.png')
+    pilt.write_to_png('Valminud kunstiteosed/' + uus_nimi)
