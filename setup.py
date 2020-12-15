ef08ccf417b0964bd6468d5f7e26d031f555ba6b
karolinkonrad <karolinkonrad@hotmail.com>
2020-12-15 23:53:13 +0200

    exe fail

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
+#https://cx-freeze.readthedocs.io/en/latest/distutils.html
+import sys
 from cx_Freeze import setup, Executable
 
-executables = [Executable('programm.py')]
+build_exe_options = {"packages": ["os", 'PIL']}
 
-setup(name='exe_art', version='0.1', description= 'progeArtProjekt', executables= executables)
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(name='progeArtProjekt', description= 'kunsti programm', options = {"build_exe": build_exe_options}, executables= [Executable('programm.py', base=base)])
