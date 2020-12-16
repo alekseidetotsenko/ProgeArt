3c3c8f03b954789a428f838e0dc0ff69d260a9fa
karolinkonrad <karolinkonrad@hotmail.com>
2020-12-16 18:01:42 +0200

    Update programm.py

--- programm.py
+++ programm.py
@@ -235,12 +235,12 @@ silt_pildi_laius = Label(raam, text = 'Laius:', bg = tausta_värv, fg = teksti_v
 silt_pildi_laius.grid(row = laiuse_sildi_rida, column = laiuse_sildi_veerg, sticky = W)
 kast_pildi_laius = Entry(raam, bg = kasti_tausta_värv, fg = teksti_värv)
 kast_pildi_laius.grid(row = laiuse_kasti_rida, column = laiuse_kasti_veerg, sticky = W)
-kast_pildi_laius.insert(0,'5940')
+kast_pildi_laius.insert(0,'594')
 
 silt_pildi_kõrgus = Label(raam, text = 'Kõrgus:', bg = tausta_värv, fg = teksti_värv)
 silt_pildi_kõrgus.grid(row = kõrguse_sildi_rida, column = kõrguse_sildi_veerg, sticky = W)
 kast_pildi_kõrgus = Entry(raam, bg = kasti_tausta_värv, fg = teksti_värv)
 kast_pildi_kõrgus.grid(row = kõrguse_kasti_rida, column = kõrguse_kasti_veerg, sticky = W)
-kast_pildi_kõrgus.insert(0,'3060')
+kast_pildi_kõrgus.insert(0,'306')
 
 raam.mainloop()
